fe3a6097aaf82d05e60a8ff77d757fb93d2b2e83
Patrick Kelly <patrick.kelly@zuora.com>
2017-03-12 12:52:36 -1000

    more/better tests

--- srxh_test.go
+++ srxh_test.go
@@ -21,14 +21,42 @@ var srxhTests = []struct {
 	{3, 3, true, []int{0, 1, 2, 3, 4, 5, 6}},
 	{0, 0, true, []int{0, 1, 2, 3, 4, 5, 6}},
 	{6, 6, true, []int{0, 1, 2, 3, 4, 5, 6}},
+
+	{0, 4, true, []int{-9823, -983, -83, -3, 0, 83, 828, 7474}},
+	{-9824, 0, false, []int{-9823, -983, -83, -3, 0, 83, 828, 7474}},
+	{7475, 8, false, []int{-9823, -983, -83, -3, 0, 83, 828, 7474}},
 }
 
+func testOneLookup(t *testing.T, key int, values []int, expectedPos int, expectedFound bool) {
+
+	actualPos, actualFound := binsrxh(key, values)
+
+	if actualPos != expectedPos || actualFound != expectedFound {
+		t.Errorf("Expected values (%d,%t) searching for %d in %s, but got (%d,%t) instead.",
+			expectedPos, expectedFound, key, fmt.Sprint(values), actualPos, actualFound)
+	}
+
+	if actualFound && values[actualPos] != key {
+		t.Errorf("Expected found value %d at position %d in %s, but see %d instead.",
+			key, actualPos, fmt.Sprint(values), values[actualPos])
+	}
+
+}
+
+// TestSrxh checks the specified results in srxhTests, including
+// out of range scenarios.
 func TestSrxh(t *testing.T) {
 	for _, tv := range srxhTests {
-		actualPos, actualFound := binsrxh(tv.key, tv.values)
-		if actualPos != tv.expectedPos || actualFound != tv.expectedFound {
-			t.Errorf("Expected values (%d,%t) searching for %d in %s, but got (%d,%t) instead.",
-				tv.expectedPos, tv.expectedFound, tv.key, fmt.Sprint(tv.values), actualPos, actualFound)
+		testOneLookup(t, tv.key, tv.values, tv.expectedPos, tv.expectedFound)
+	}
+}
+
+// TestAllValues tests that every value can be correctly found
+// in each of the test values slices.
+func TestAllValues(t *testing.T) {
+	for _, tv := range srxhTests {
+		for expectedPos, val := range tv.values {
+			testOneLookup(t, val, tv.values, expectedPos, true)
 		}
 	}
 }
